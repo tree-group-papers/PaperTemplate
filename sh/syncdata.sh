61580f9d3d106060a54c97a13baa715ed8f513fb
treedoug <tree.doug@byu.edu>
2019-04-22 16:43:06 -0600

    Testing syncdata.sh

--- sh/syncdata.sh
+++ sh/syncdata.sh
@@ -3,6 +3,9 @@
 # Notes:
 #   * This file is run as a pre-push hook, it must be configured properly.
 #   * Do not move or rename this file, or it will break the symlink in .git/hooks
+#   * Files in data/ are not part of the repo (they are ignored via .gitignore), 
+#     This means updating these files will not trigger a need to run a git commit.
+#     However, when a push is done, they will be backed up.
 
 repopath=$(git rev-parse --show-toplevel)
 reponame=$(basename ${repopath})
