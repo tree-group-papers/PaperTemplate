dbe5b666256a661b6b39c22ffde7ec5da6f12d55
treedoug <tree.doug@byu.edu>
2022-01-31 09:31:19 -0700

    Updated doublewide figure with instructions.

--- figures/fig-doublewide/fig-doublewide.py
+++ figures/fig-doublewide/fig-doublewide.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+## ------------------------------------------------------------------------
+## This script can be useful for combining subfigures into a single figure.
+## It creates a latex file and then compiles it to either a pdf or eps.
+## 
+## Some notes:
+## * Preamble geometry sets the size of the overall figure (in inches).
+##   - Most journals have figures that are 3.25 inches wide for a single column
+##     and 6.25 inches for a double column.
+##   - I typically use a 4x3 image aspect ratio: 3.25 in wide and 2.44 in tall
+##   - This makes a double-wide figure: 6.5in x 2.44 and a double tall figure: 
+##     3.25in x 4.88 in.
+## * Picture command and "put" commands are used to place subfigures
+##   - Must make sure the argument of the picture command matches the geometry
+##     call in the preamble.
+## * Can add text labels like (a), (b), etc. using a put command, see below
+## * Uses a sans serif font
+## * Compile with pdflatex for a pdf; Compile with latex for an eps file.
+## * Creates a figure that is named the same as the script.
+## ------------------------------------------------------------------------
+
 import os
 import subprocess
 
@@ -7,7 +27,7 @@ basename = os.path.splitext(__file__)[0]
 
 f1 = open(basename+'.tex', 'w')
 f1.writelines("""\
-\\documentclass[10pt]{article}
+\\documentclass[11pt]{article}
 
 %Preamble
 \\usepackage[margin=0in,paperwidth=6.5in,paperheight=2.44in]{geometry}
@@ -33,16 +53,12 @@ f1.writelines("""\
 )
 f1.close();
 
-#  \\put(2.375,2.30){(b)}
-#  \\put(4.050,2.30){(c)}
-#  \\put(5.675,2.30){(d)}
+# for eps
+#subprocess.run(['latex', basename+'.tex'])
+#subprocess.run(['dvips', '-t', 'unknown', basename+'.dvi', '-E', '-o', basename+'.eps'])
+#subprocess.run(['rm', basename+'.tex', basename+'.aux', basename+'.log', basename+'.dvi'])
 
-subprocess.run(['latex', basename+'.tex'])
+# for pdf
 subprocess.run(['pdflatex', basename+'.tex'])
-subprocess.run(['dvips', '-t', 'unknown', basename+'.dvi', '-E', '-o', basename+'.eps'])
-subprocess.run(['rm', basename+'.tex', basename+'.aux', basename+'.log', basename+'.dvi'])
-
-#pdflatex $name.tex
-#rm $name.tex $name.aux $name.log
-#
+subprocess.run(['rm', basename+'.tex', basename+'.aux', basename+'.log'])
 
